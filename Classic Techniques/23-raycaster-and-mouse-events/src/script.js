9ba4784c3bae5467d5d30144900757be6cc28cb1
Marko Ma <markomadev@gmail.com>
2023-08-13 01:20:58 +0800

    raycasting with model

--- Classic Techniques/23-raycaster-and-mouse-events/src/script.js
+++ Classic Techniques/23-raycaster-and-mouse-events/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 THREE.ColorManagement.enabled = false
 
@@ -40,6 +41,36 @@ object3.position.x = 2
 object3.updateMatrixWorld();
 
 scene.add(object1, object2, object3)
+
+/**
+ * Models
+ */
+
+let model;
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.load('/models/Duck/glTF/Duck.gltf', (gltf) => {
+  model = gltf.scene
+  scene.add(model);
+})
+
+/**
+ * Lighting
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
 
 /**
  * Raycasting
@@ -151,11 +182,12 @@ const castRay = () => {
 
   // raycaster.set(rayOrigin, rayDirection);
 
+
+  // test sphere objects
   const objectsToTest = [object1, object2, object3]
 
   for (const object of objectsToTest) {
     object.material.color.set('#ff0000')
-    object.visible = true;
   }
 
   const intersects = raycaster.intersectObjects(objectsToTest);
@@ -174,6 +206,15 @@ const castRay = () => {
 
   for (const intersect of intersects) {
     intersect.object.material.color.set('#0000ff')
+  }
+
+  // test model
+  if (model) {
+    model.scale.set(1, 1, 1);
+    const modelIntersects = raycaster.intersectObject(model); // three js raycaster by default test model group and its children meshes recursively. In the old version it only takes a mesh but not group meaning that we would need to take the mesh out of model and intersect it directly. 
+    if (modelIntersects.length) {
+      model.scale.set(2, 2, 2);
+    }
   }
 }
 
